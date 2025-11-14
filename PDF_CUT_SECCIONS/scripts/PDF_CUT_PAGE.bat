e55804dc11c3a00b86f6d4f74eaf0ce3517fd202
Kevin <130321650+Kevin-Cobos@users.noreply.github.com>
2025-11-14 06:17:31 +0100

    Modify PDF_CUT_PAGE.bat for relative path and exit
    Updated the script to change the working directory to a relative path and added an exit command.

--- PDF_CUT_SECCIONS/scripts/PDF_CUT_PAGE.bat
+++ PDF_CUT_SECCIONS/scripts/PDF_CUT_PAGE.bat
@@ -1,10 +1,11 @@
 @echo off
 REM Cambia el directorio a la ubicación del script Python
-cd "C:\Users\PC\Documents\PROJECTES\Proyecto Cono\_Scripts\PDF_cut_pag"
+cd ".\_Scripts\PDF_cut_pag"
 
 REM Ejecuta el script Python usando pythonw.exe para evitar que aparezca la ventana de la consola.
 REM Si pythonw.exe no funciona, puedes probar con "python.exe" en su lugar, pero aparecerá una ventana de consola.
 start "" pythonw.exe PDF_CUT_PAGE.py
 
 REM Cierra esta ventana de comando (el archivo .bat) inmediatamente después de lanzar la aplicación Python.
+
 exit
